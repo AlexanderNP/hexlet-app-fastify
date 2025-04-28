c253cc5ccd999d09b7e5126ac71c82d80c34d8a2
Alexander Enin <enin02@mail.ru>
2025-04-28 14:56:51 +0500

    feat: Добавлены GET и POST методы для приветсвия

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import fastify from 'fastify';
 const app = fastify();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/hello', (req, res) => {
+  const name = req.query?.name ?? "World";
+  res.send(`Hello ${name}!`);
 });
+
+app.post("/user", (req, res) => {
+  res.send(`Holla ${JSON.stringify(req.body.name)}`)
+})
+
 
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
